27a3c72831008d3be597bd762f2ae1b06ed6994b
Freider Achic <freider8998@gmail.com>
2023-01-24 11:48:13 -0500

    add clickjacking example

---
+++ Security/Clickjacking/main.js
@@ -0,0 +1,5 @@
+const sendToServer = ()=>{
+    const email = document.getElementById('fakeEmail').value;
+    const pass = document.getElementById('fakePassword').value;
+    alert(`${email}:${pass}`)
+}
